22a8f7206a3e650996f362a26d648ac5df9da6ba
xus <dyxushuai@gmail.com>
2014-07-22 11:25:09 +0800

    debug

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,18 +18,25 @@ describe "UserPages" do
 
   describe "signup" do
     before { visit signup_path }
-    let(:submit) { "Create my account" }
+    let(:submit) { "注册" }
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "用户名", with: "Example User"
+        fill_in "邮箱", with: "user@example.com"
+        fill_in "密码", with: "foobar"
+        fill_in "确认密码", with: "foobar"
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
